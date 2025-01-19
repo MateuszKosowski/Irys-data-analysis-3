41cf47907da98fc5939483eb27ba898e50d6236f
MateuszKosowski <mateuszkosowski03@gmail.com>
2025-01-19 18:14:51 +0100

    Change normalize method

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import MinMaxScaler
 
 # Wczytaj plik csv
 file_test = './dane/data3_test.csv'
@@ -27,8 +28,9 @@ def analyze_knn(feature_columns, description, figure_number):
     y_test_f = data_test['Gatunek'].values
 
     # Normalizacja danych
-    x_train_norm = (x_train - x_train.min()) / (x_train.max() - x_train.min())
-    x_test_norm = (x_test - x_test.min()) / (x_test.max() - x_test.min())
+    scaler = MinMaxScaler()
+    x_train_norm = scaler.fit_transform(x_train)
+    x_test_norm= scaler.transform(x_test) # Dane testowe są normalizowane zgodnie z tym samym odniesieniem, co dane treningowe, co zapewnia spójność.
 
     # Lista do przechowywania dokładności modelu dla różnych wartości k
     accuracies = []
@@ -46,23 +48,24 @@ def analyze_knn(feature_columns, description, figure_number):
         # Sprawdzenie dokładności modelu
         y_pred = knn.predict(x_test_norm)
         accuracy = accuracy_score(y_test_f, y_pred) # accuracy zwraca od 0 do 1, gdzie 1 oznacza 100% dokładności
-        accuracies.append(accuracy)
+        accuracies.append(accuracy * 100)
+
 
     # Wykres dokładności
     plt.figure(figure_number, figsize=(10, 6), tight_layout=True)
     plt.bar(k_range, accuracies)
     plt.title(description, fontsize=16, pad=15)
     plt.xlabel('Liczba sasiadow (k)', fontsize=14, labelpad=15)
-    plt.ylabel('Dokladnosc', fontsize=14, labelpad=15)
+    plt.ylabel('Dokladnosc (%)', fontsize=14, labelpad=15)
     plt.xticks(k_range, fontsize=12)
     plt.grid(axis='y', which='both')
-    plt.ylim(0.60, 1.05)
-    plt.yticks(np.arange(0.60, 1.05, 0.05), fontsize=12)
+    plt.ylim(60, 105)
+    plt.yticks(np.arange(60, 105, 5), fontsize=12)
 
     # Najlepsza wartość k
     print(f'--- Wyniki dla cech {feature_columns} ---')
-    best_k = k_range[np.argmax(accuracies)]    # Zwraca indeks maksymalnej wartości z listy accuracies, jeśli są remisy to zwraca pierwszy
-    print(f'Najlepsze k: {best_k}, dokladnosc: {max(accuracies):.2f}')
+    best_k = k_range[np.argmax(accuracies)]  # Zwraca indeks maksymalnej wartości z listy accuracies, jeśli są remisy to zwraca pierwszy
+    print(f'Najlepsze k: {best_k}, dokladnosc: {max(accuracies):.2f}%')
 
     # Macierz pomyłek dla najlepszego k
     knn = KNeighborsClassifier(n_neighbors=best_k, weights='distance')
